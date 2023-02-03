aa6bd7c06f67035b18108086d266ba37485983ab
Sarangi Sunil <124140871+SarangiSunil@users.noreply.github.com>
2023-02-03 23:11:00 +0530

    Create smapleinput.py

---
+++ smapleinput.py
@@ -0,0 +1,9 @@
+print("Enter student details:")
+name = input("Name: ")
+roll_number = input("Roll Number: ")
+marks = input("Marks: ")
+
+print("\nStudent Details:")
+print("Name:", name)
+print("Roll Number:", roll_number)
+print("Marks:", marks)
