c9933e3cd49082561eba551f87ef68883e342c04
Sarangi Sunil <124140871+SarangiSunil@users.noreply.github.com>
2023-02-03 23:27:26 +0530

    Update separated_by_spaces.py

--- separated_by_spaces.py
+++ separated_by_spaces.py
@@ -1,5 +1,3 @@
-python
-Copy code
 numbers = [int(x) for x in input("Enter a list of numbers separated by spaces: ").split()]
 num = int(input("Enter the number to be inserted: "))
 pos = int(input("Enter the position to insert the number: "))
